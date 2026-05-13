73f926c8bd1d67af8eb1c79a8fb6b96629166760
Furqan Ali Jumnani <furqanali.cs21@gmail.com>
2026-05-13 11:57:14 +0500

    Test basic api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,11 +2,10 @@ export default async function handler(
   req: any,
   res: any,
 ) {
-  const { pathname } = new URL(req.url ?? "/", "http://localhost");
   res.status(200).json({
     ok: true,
-    message: "Catch-all function works!",
-    path: pathname,
+    message: "Root API function works!",
+    url: req.url,
     method: req.method,
   });
 }
